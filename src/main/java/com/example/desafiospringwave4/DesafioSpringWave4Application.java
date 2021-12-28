ae4a1b0b3fb9bf13427e1ff3ac059f00cd854974
netto-meli <fernando.netto@mercadolivre.com>
2021-12-28 17:13:10 -0300

    v 0.1 Desafio Spring

---
+++ src/main/java/com/example/desafiospringwave4/DesafioSpringWave4Application.java
@@ -0,0 +1,13 @@
+package com.example.desafiospringwave4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringWave4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSpringWave4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/desafiospringwave4/DesafioSpringWave4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiospringwave4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringWave4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

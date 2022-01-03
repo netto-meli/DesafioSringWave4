c70c5b2a71308fdf65d2b9560bd15ad8412b1809
netto-meli <fernando.netto@mercadolivre.com>
2022-01-03 09:54:16 -0300

    # Conflicts: #	README.md #	clientes.json #	pedidos.json #	src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/controller/ClienteController.java #	src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/repository/OurRepository.java #	src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/service/ClienteService.java #	src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/service/PedidoService.java

--- src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/service/PedidoService.java
+++ src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/service/PedidoService.java
@@ -9,6 +9,7 @@ import br.com.meli.bootcamp.wave4.grupo9.desafio.spring.repository.PedidoReposit
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -70,40 +71,24 @@ public class PedidoService {
             switch (number) {
                 case 0:
                     return pedidoRepository.listagem().stream()
-                            .sorted((p1, p2) -> sortCliente(p1, p2))
+                            .sorted(Comparator.comparing(Pedido::getId).reversed())
                             .collect(Collectors.toList());
                 case 1:
                     return pedidoRepository.listagem().stream()
-                            .sorted((p2, p1) -> sortCliente(p2, p1))
+                            .sorted(Comparator.comparing(Pedido::getId))
                             .collect(Collectors.toList());
                 case 2:
                     return pedidoRepository.listagem().stream()
-                            .sorted((p1, p2) -> p1.getValorTotal().compareTo(p2.getValorTotal()))
+                            .sorted(Comparator.comparing(Pedido::getValorTotal))
                             .collect(Collectors.toList());
                 case 3:
                     return pedidoRepository.listagem().stream()
-                            .sorted((p2, p1) -> p2.getValorTotal().compareTo(p1.getValorTotal()))
+                            .sorted(Comparator.comparing(Pedido::getValorTotal).reversed())
                             .collect(Collectors.toList());
             }
         } catch (Exception e){
             throw new NotFoundExceptionProduct("Erro na Ordenação");
         }
         return null;
-    }
-
-    /***
-     *
-     * @param p1 p1
-     * @param p2 p2
-     * 0 - Id Cliente crescente
-     * 1 - Id Cliente decrescente
-     * 2 - Valor total crescente
-     * 3 - Valor Total descrecente
-     * @return Método que faz ordenação da lista de acordo com o id do cliente
-     */
-    private int sortCliente(Pedido p1, Pedido p2){
-        Cliente cli1 = clienteRepository.get(p1.getIdCliente());
-        Cliente cli2 = clienteRepository.get(p2.getIdCliente());
-        return cli1.getNome().compareTo(cli2.getNome());
     }
 }

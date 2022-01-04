bde14c3c47d87db078173c978e53dc6ca79c8e29
lassuncao <leonardo.assuncao@mercadolivre.com>
2022-01-04 12:21:37 -0300

    Fazendo o projeto rodar

--- src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/dto/ProdutoDTO.java
+++ src/main/java/br/com/meli/bootcamp/wave4/grupo9/desafio/spring/dto/ProdutoDTO.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /*** DTO par serialização de Produto
  *
